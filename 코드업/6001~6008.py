af616848681a715cadf97d2c74e1ad39f469a796
shinyehwan <jghisjl007@naver.com>
2022-02-09 14:57:06 +0900

    2022.2.9 CodeUp

---
+++ 코드업/6001~6008.py
@@ -0,0 +1,16 @@
+print('Hello')
+
+print('Hello World')
+
+print('Hello')
+print('World')
+
+print("'Hello'")
+
+print('"Hello World"')
+
+print('\"!@#$%^&*()\'')
+
+print('\"C:\\Download\\\'hello\'.py\"')
+
+print('print("Hello\\nWorld")')
