e0731cdf1be22af79753ecf9fcd76aa7639f137d
shinyehwan <jghisjl007@naver.com>
2022-02-11 23:02:51 +0900

    2020.2.10

---
+++ 코드업/2.py
@@ -0,0 +1,37 @@
+# # 6009
+# c = input()
+# print(c)
+
+# # 6010
+# c = input()
+# c = int(c)
+# print(c)
+
+# # 6011
+# f = input()
+# f = float(f)
+# print(f)
+
+# # 6012
+# a = input()
+# b = input()
+# print(a)
+# print(b)
+
+# # 6013
+# a = input()
+# b = input()
+# print(b)
+# print(a)
+
+# 6014
+a = input()
+a = float(a)
+print(a)
+print(a)
+print(a)
+
+# 6015
+a, b = input.split()
+print(a)
+print(b)
