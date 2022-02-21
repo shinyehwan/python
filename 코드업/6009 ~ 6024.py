6c63a4cd8f30b5243176d2ab62aab5bbbdf5e8ff
shinyehwan <jghisjl007@naver.com>
2022-02-21 15:36:43 +0900

    2022.2.21

---
+++ 코드업/6009 ~ 6024.py
@@ -0,0 +1,78 @@
+# # 6009
+# c = input()
+# print(c)
+
+# # 6010
+# c = input()
+# c = int(c)
+# print(c)
+
+# # 6011
+# f = input()
+# f = float(f)
+# print(f)
+
+# # 6012
+# a = input()
+# b = input()
+# print(a)
+# print(b)
+
+# # 6013
+# a = input()
+# b = input()
+# print(b)
+# print(a)
+
+# 6014
+# a = input()
+# a = float(a)
+# print(a)
+# print(a)
+# print(a)
+
+# 6015
+# a, b = input.split()
+# print(a)
+# print(b)
+
+# 6016
+# a, b = input().split() 
+# print(b, a)
+
+# 6017
+# s = input()
+# print(s, s, s)
+
+# 6018
+# a, b = input().split(':')
+# print(a, b, sep=':')
+
+# 6019
+# a, b, c = input().split('.')
+# print(c,b,a, sep='-')
+
+# 6020
+# a,b = input().split('-')
+# print(a,b,sep='')
+
+# 6021
+# s = input()
+# print(s[0])
+# print(s[1])
+# print(s[2])
+# print(s[3])
+# print(s[4])
+
+# 6022
+# s = input() 
+# print(s[0:2], s[2:4], s[4:6], sep=' ') 
+
+# 6023
+# s = input().split(':')
+# print(s[1])
+
+# 6024
+# w1, w2 = input().split()
+# s = w1 + w2
+# print(s)
