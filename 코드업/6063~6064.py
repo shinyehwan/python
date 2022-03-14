c0df59e7883bb7beb0f7d719c10e79da5c7caad8
shinyehwan <jghisjl007@naver.com>
2022-03-14 21:42:13 +0900

    2022.3.14

---
+++ 코드업/6063~6064.py
@@ -0,0 +1,9 @@
+# 6063
+# a, b = map(int, input().split())
+# c = (a if a>b else b)
+# print(c)
+
+# 6064
+# a, b, c = map(int, input().split())
+# d = (a if a>b else b) if ((a if a>b else b)>c)else c
+# print(d)
---
+++ 코드업/6065 ~ 6070.py
@@ -0,0 +1,77 @@
+# 6065
+# a,b,c = map(int, input().split())
+# if a%2 == 0 :
+#     print(a)
+# if b%2 == 0 :
+#     print(b)
+# if c%2 == 0 :
+#     print(c)
+
+# 6066
+# a,b,c = map(int, input().split())
+# if a%2 == 0:
+#     print('even')
+# else :
+#     print('odd')
+# if b%2 == 0:
+#     print('even')
+# else :
+#     print('odd')
+# if c%2 == 0:
+#     print('even')
+# else :
+#     print('odd')
+
+# 6067
+# a = input()
+# a = int(a)
+# if a < 0:
+#     if a%2 == 0:
+#         print('A')
+#     else:
+#         print('B')
+# else:
+#     if a%2 ==0:
+#         print('C')
+#     else:
+#         print('D')
+
+# 6068
+# a = input()
+# a = int(a)
+
+# if 90<=a<=100:
+#     print('A')
+# elif 70<=a<=89:
+#     print('B')
+# elif 40<=a<=69:
+#     print('C')
+# else:
+#     print('D')
+
+# 6069
+# a = input()
+
+# if a == 'A':
+#     print('best!!!')
+# elif a == 'B':
+#     print('good!!')
+# elif a == 'C':
+#     print('run!')
+# elif a == 'D':
+#     print('slowly~')
+# else:
+#     print('what?')
+
+# 6070
+a = input()
+a = int(a)
+
+if a//3 == 1:
+    print('spring')
+elif a//3 == 2:
+    print('summer')
+elif a//3 == 3:
+    print('fall')
+else:
+    print('winter')
