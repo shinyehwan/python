c5ca9fda64d7fe396c198f9664e8c4cc05b305ee
shinyehwan <jghisjl007@naver.com>
2022-03-15 15:45:51 +0900

    2022.3.15

---
+++ 코드업/6071 ~ 6076.py
@@ -0,0 +1,18 @@
+# 6071
+# while True:
+#     a = input()
+#     a = int(a)
+#     if a == 0:
+#         break
+#     else:
+#         print(a)
+
+# 6072
+
+n = int(input())
+while True:
+    print(n)
+    n -= 1
+    if n == 0:
+        break
+    
